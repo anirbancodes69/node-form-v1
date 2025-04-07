22ad098024c763b385d7aa9082dbc04b5cc78b63
Anirban <anirbancodes@gmail.com>
2025-04-07 23:34:04 +0530

    second commit

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ const server = http.createServer((req, res) => {
 });
 
 // Start the server
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on 127.0.0.1:8000");
+server.listen(8000, () => {
+  console.log("Listening to requests on PORT: 8000");
 });
 
 // -----------------------------------------------------------------------------------------------------
